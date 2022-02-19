0d71e85e86eca79ea6e1a178f6f61215eaf16c84
Your Name <87517930+Sa1D1111@users.noreply.github.com>
2022-02-18 16:10:07 -0800

    .
    Signed-off-by: Your Name <87517930+Sa1D1111@users.noreply.github.com>

--- Test.py
+++ Test.py
@@ -1,17 +1,16 @@
 import turtle
 
 t = turtle.Turtle()
-
 style = ("times", 20, 'bold')
 t.write("wait for imput", font=style, align="center")
 t.right(-90)
 
-num = int(input("how manyarrows do you want?"))
+num = int(input("how many arrows do you want?"))
 t.clear()
 
 t.width(5)
 
-for i in range(num):
+for i in range(11):
     t.forward(100)
     t.left(45)
     t.backward(20)
@@ -19,6 +18,46 @@ for i in range(num):
     t.right(90)
     t.backward(20)
     t.forward(20)
-from turtle import *
-win = Turtle()
-done()
+for i in range(12):
+    t.forward(100)
+    t.right(45)
+    t.left(20)
+    t.forward(20)
+    t.left(90)
+    t.backward(20)
+    t.forward(20)
+    
+for i in range(12):
+    t.right(100)
+    t.right(45)
+    t.backward(20)
+    t.forward(20)
+    t.left(90)
+    t.right(20)
+    t.backward(20)
+    
+for i in range(11):
+    t.forward(100)
+    t.left(45)
+    t.backward(20)
+    t.forward(20)
+    t.right(90)
+    t.backward(20)
+    t.forward(20)
+for i in range(12):
+    t.forward(100)
+    t.right(45)
+    t.left(20)
+    t.forward(20)
+    t.left(90)
+    t.backward(20)
+    t.forward(20)
+    
+for i in range(num):
+    t.right(100)
+    t.right(45)
+    t.backward(20)
+    t.forward(20)
+    t.left(90)
+    t.right(20)
+    t.backward(20)
